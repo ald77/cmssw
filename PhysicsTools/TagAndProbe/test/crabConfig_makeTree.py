f98f355595bfa5038fbc72abb9db719a68b28142
Adam Dishaw <aldishaw@gmail.com>
2015-07-27 23:34:59 +0200

    Add files to submit CRAB jobs

---
+++ PhysicsTools/TagAndProbe/test/crabConfig_makeTree.py
@@ -0,0 +1,20 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = 'makeTree'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'makeTree.py'
+
+config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/MINIAODSIM'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 10
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = True
+config.Data.publishDataName = 'CRAB3_TnP_tree'
+
+config.Site.storageSite = 'T2_US_UCSD'
