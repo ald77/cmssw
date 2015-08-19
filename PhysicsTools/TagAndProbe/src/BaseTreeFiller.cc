2666d7f8c9d780e6008bec1050694bfcd29c3187
Adam Dishaw <aldishaw@gmail.com>
2015-08-18 22:39:42 -0700

    Add basic cleaned jet quantities to ntuples

--- PhysicsTools/TagAndProbe/src/BaseTreeFiller.cc
+++ PhysicsTools/TagAndProbe/src/BaseTreeFiller.cc
@@ -3,6 +3,8 @@
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 
 #include "PhysicsTools/TagAndProbe/interface/ColinsSoperVariables.h"
+#include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/Math/interface/deltaR.h"
 
 #include <TList.h>
 #include <TObjString.h>
@@ -81,6 +83,19 @@ tnp::BaseTreeFiller::BaseTreeFiller(const char *name, const edm::ParameterSet& i
 	tree_->Branch("event_BeamSpot_x"   ,&mBSx_     ,"mBSx/F");
 	tree_->Branch("event_BeamSpot_y"   ,&mBSy_     ,"mBSy/F");
 	tree_->Branch("event_BeamSpot_z"   ,&mBSz_     ,"mBSz/F");
+      }
+
+      addJetVariablesInfo_ = iConfig.existsAs<edm::InputTag>("allProbes") && iConfig.existsAs<edm::InputTag>("jetCollection");
+      if(addJetVariablesInfo_){
+	probesToken_ = iC.consumes<reco::CandidateView>(iConfig.getParameter<edm::InputTag>("allProbes"));
+	jetsToken_ = iC.consumes<pat::JetCollection>(iConfig.getParameter<edm::InputTag>("jetCollection"));
+
+	jet_pt_cut_ = iConfig.getParameter<double>("jet_pt_cut");
+	jet_eta_cut_ = iConfig.getParameter<double>("jet_eta_cut");
+	match_delta_r_ = iConfig.getParameter<double>("match_delta_r");
+
+	tree_->Branch("event_ht", &mht_, "mht/F");
+	tree_->Branch("event_njets", &mnjets_, "mnjets/F");
       }
     }
 
@@ -206,6 +221,30 @@ void tnp::BaseTreeFiller::init(const edm::Event &iEvent) const {
 	mpfPhi_ = met.phi();
 	mpfSumET_ = met.sumEt();
  
+	if(addJetVariablesInfo_){
+	  mnjets_ = 0.;
+	  mht_ = 0.;
+	  edm::Handle<reco::CandidateView> probes;
+	  iEvent.getByToken(probesToken_, probes);
+	  edm::Handle<pat::JetCollection> jets;
+	  iEvent.getByToken(jetsToken_, jets);
+
+	  for(auto jet = jets->begin();
+	      jet != jets->end();
+	      ++jet){
+	    double pt = jet->pt();
+	    if(pt < jet_pt_cut_ || fabs(jet->eta())>jet_eta_cut_) continue;
+	    bool matched_to_electron = false;
+	    for(auto ele = probes->begin();
+		ele != probes->end();
+		++ele){
+	      if(deltaR(*jet, *ele) < match_delta_r_) matched_to_electron = true;
+	    }
+	    if(matched_to_electron) continue;
+	    mnjets_ += 1.;
+	    mht_ += pt;
+	  }
+	}
     }
 
 }
