d3fbd053d1d365bb8a726b36f79714497b846ce5
Adam Dishaw <aldishaw@gmail.com>
2015-09-10 13:24:35 -0700

    Use correct input file

--- PhysicsTools/TagAndProbe/test/normalize_files.sh
+++ PhysicsTools/TagAndProbe/test/normalize_files.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-cp current/*mc*.root .
+cp current/TnPTree_mc.root .
 
-python utilities/normalizeMCWeights.py -i TnPTree_mc_neg.root -d GsfElectronToID; mv *norm*.root current/TnPTree_id_norm.root;
-python utilities/normalizeMCWeights.py -i TnPTree_mc_neg.root -d VetoElectronToIso; mv *norm*.root current/TnPTree_veto_norm.root;
-python utilities/normalizeMCWeights.py -i TnPTree_mc_neg.root -d LooseElectronToIso; mv *norm*.root current/TnPTree_loose_norm.root;
-python utilities/normalizeMCWeights.py -i TnPTree_mc_neg.root -d MediumElectronToIso; mv *norm*.root current/TnPTree_medium_norm.root;
-python utilities/normalizeMCWeights.py -i TnPTree_mc_neg.root -d TightElectronToIso; mv *norm*.root current/TnPTree_tight_norm.root;
+python utilities/normalizeMCWeights.py -i TnPTree_mc.root -d GsfElectronToID; mv *norm*.root current/TnPTree_id_norm.root;
+python utilities/normalizeMCWeights.py -i TnPTree_mc.root -d VetoElectronToIso; mv *norm*.root current/TnPTree_veto_norm.root;
+python utilities/normalizeMCWeights.py -i TnPTree_mc.root -d LooseElectronToIso; mv *norm*.root current/TnPTree_loose_norm.root;
+python utilities/normalizeMCWeights.py -i TnPTree_mc.root -d MediumElectronToIso; mv *norm*.root current/TnPTree_medium_norm.root;
+python utilities/normalizeMCWeights.py -i TnPTree_mc.root -d TightElectronToIso; mv *norm*.root current/TnPTree_tight_norm.root;
 
-rm *.root
+rm -f *.root
