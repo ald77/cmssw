49d5274d4b594ac875206e03b5e686cef65cd139
Adam Dishaw <aldishaw@gmail.com>
2015-10-06 19:55:40 +0200

    Fix configuration errors

--- PhysicsTools/TagAndProbe/test/makeTree.py
+++ PhysicsTools/TagAndProbe/test/makeTree.py
@@ -33,9 +33,6 @@ options['DEBUG']                   = cms.bool(False)
 
 from PhysicsTools.TagAndProbe.treeMakerOptions_cfi import *
 
-import PhysicsTools.TagAndProbe.treeMakerOptionsAdam_cfi as adam
-adam.AdjustOptions(options, varOptions)
-
 if (varOptions.isMC):
     options['INPUT_FILE_NAME']     = "/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v2/00000/00C4781D-6B08-E511-8A0A-0025905A6084.root"
     options['OUTPUT_FILE_NAME']    = "TnPTree_mc.root"
@@ -54,6 +51,9 @@ else:
     options['HLTFILTERTOMEASURE']  = cms.vstring("")
     options['GLOBALTAG']           = 'GR_P_V56'
     options['EVENTSToPROCESS']     = cms.untracked.VEventRange()
+
+import PhysicsTools.TagAndProbe.treeMakerOptionsAdam_cfi as adam
+adam.AdjustOptions(options, varOptions)
 
 ###################################################################
 
