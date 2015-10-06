66183017f85d0a9d68be67dcc802ee731876f111
Adam Dishaw <aldishaw@gmail.com>
2015-10-06 14:58:10 +0200

    Update samples and json for CRAB jobs

--- PhysicsTools/TagAndProbe/test/crabConfig_data_makeTree.py
+++ PhysicsTools/TagAndProbe/test/crabConfig_data_makeTree.py
@@ -9,12 +9,12 @@ config.General.transferLogs = True
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'makeTreeData.py'
 
-config.Data.inputDataset = '/SingleElectron/Run2015B-PromptReco-v1/MINIAOD'
+config.Data.inputDataset = '/SingleElectron/Run2015C-PromptReco-v1/MINIAOD'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 20
-config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-251883_13TeV_PromptReco_Collisions15_JSON_v2.txt'
-config.Data.runRange = '246908-251883'
+config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-257599_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
+config.Data.runRange = '246908-257599'
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication = True
 config.Data.publishDataName = 'CRAB3_TnP_tree'
