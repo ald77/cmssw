823a5c314981d8d4faebd794076f64850b35fea4
Adam Dishaw <aldishaw@gmail.com>
2015-10-07 13:12:36 +0200

    Update script to normalize MC

--- PhysicsTools/TagAndProbe/test/normalize_files.sh
+++ PhysicsTools/TagAndProbe/test/normalize_files.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
 cp current/TnPTree_mc.root .
+cp current/TnPTree_data.root .
 
-python utilities/normalizeMCWeights.py -i TnPTree_mc.root -d GsfElectronToID; mv *norm*.root current/TnPTree_id_norm.root;
-python utilities/normalizeMCWeights.py -i TnPTree_mc.root -d VetoElectronToIso; mv *norm*.root current/TnPTree_veto_norm.root;
-python utilities/normalizeMCWeights.py -i TnPTree_mc.root -d LooseElectronToIso; mv *norm*.root current/TnPTree_loose_norm.root;
-python utilities/normalizeMCWeights.py -i TnPTree_mc.root -d MediumElectronToIso; mv *norm*.root current/TnPTree_medium_norm.root;
-python utilities/normalizeMCWeights.py -i TnPTree_mc.root -d TightElectronToIso; mv *norm*.root current/TnPTree_tight_norm.root;
+python utilities/normalizeMCWeights.py -m TnPTree_mc.root -d TnPTree_data.root; mv *norm*.root current/TnPTree_mc_norm.root;
+#python utilities/normalizeMCWeights.py -m TnPTree_mc.root -d TnPTree_data.root -n; mv *norm*.root current/TnPTree_mc_norm.root;
 
 rm -f *.root
