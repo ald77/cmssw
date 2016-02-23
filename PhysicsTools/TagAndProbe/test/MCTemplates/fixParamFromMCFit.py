5ba29ed35d529d666c2acf5104a5cccf43b887d6
Adam Dishaw <aldishaw@gmail.com>
2016-02-23 10:54:02 -0800

    Fix parametric "template" names

--- PhysicsTools/TagAndProbe/test/MCTemplates/fixParamFromMCFit.py
+++ PhysicsTools/TagAndProbe/test/MCTemplates/fixParamFromMCFit.py
@@ -2,6 +2,7 @@ import ROOT
 from optparse import OptionParser
 from os import listdir
 from os.path import isfile, join
+from string import replace
 
 def GetDirName(file_name):
     if file_name == "eff_mc_foid2d.root":
@@ -51,8 +52,23 @@ def GetFitName(filename, keyname):
     pos = keyname.rfind("__")
     if pos == -1:
         return None
+    elif "RelAct" in keyname:
+        return None
     else:
-        return keyname[pos+2:]
+        output = keyname[pos+2:]
+        if keyname.find("probe_sc_abseta_bin0") != -1:
+            output = replace(output, "0p0To1p442", "0p0To0p8")
+            output = replace(output, "0p0To2p5", "0p0To0p8")
+        elif keyname.find("probe_sc_abseta_bin1") != -1:
+            output = replace(output, "0p0To1p442", "0p8To1p442")
+            output = replace(output, "0p0To2p5", "0p8To1p442")
+        elif keyname.find("probe_sc_abseta_bin3") != -1:
+            output = replace(output, "1p566To2p5", "1p566To2p0")
+            output = replace(output, "0p0To2p5", "1p566To2p0")
+        elif keyname.find("probe_sc_abseta_bin4") != -1:
+            output = replace(output, "1p566To2p5", "2p0To2p5")
+            output = replace(output, "0p0To2p5", "2p0To2p5")
+        return output
     
 def GetParamString(name, params):
     for p in xrange(params):
@@ -69,7 +85,9 @@ def main(options):
     with open("../python/commonFit.py", "w") as out:
         out.write("import FWCore.ParameterSet.Config as cms\n")
         out.write("\n")
+        out.write("block_0 = cms.PSet(\n")
 
+        entry = 0
         block = 0
 
         files = [ filename for filename in listdir(options.input) if isfile(join(options.input,filename)) and "eff_mc_" in filename ]
@@ -79,34 +97,46 @@ def main(options):
             directory.cd()
             keys = directory.GetListOfKeys()
             for key in keys:
-                if "__" in key.GetName():
-                    subdirectory = directory.Get(key.GetName())
-                    subdirectory.cd();
-                    results = subdirectory.Get("fitresults")
-                    params = results.floatParsFinal()
-                    if block%100:
-                        if block!=0:
-                            out.write(")\n")
-                        out.write("block_"+str(block)+" = cms.PSet(\n")
-                        maxblock = block
-                        block += 1
-                    out.write(GetFitName(filename, key.GetName())+" = cms.vstring(\n")
-                    out.write("\"RooDoubleCBFast::signalResPass(mass,meanP[0.0,-10.000,10.000],sigmaP[0.956,0.00,10.000],alphaP1[0.999, 0.0,50.0],nP1[1.405,0.000,50.000],alphaP2[0.999,0.0,50.0],nP2[1.405,0.000,50.000])\",\n")
-                    out.write("\"RooDoubleCBFast::signalResFail(mass,meanF[0.0,-10.000,10.000],sigmaF[3.331,0.00,10.000],alphaF1[1.586, 0.0,50.0],nF1[0.464,0.000,20.00],alphaF2[1.586,0.0,50.0],nF2[0.464,0.000,20.00])\",\n")
-                    out.write("\"RooBreitWigner::signalPassBW(mass, mZpass[91.1876,80.,100.],sigmaZpass[2.4952,0.001,10.])\",\n")
-                    out.write("\"RooBreitWigner::signalFailBW(mass, mZfail[91.1876,80.,100.],sigmaZfail[2.4952,0.001,10.])\",\n")
-                    out.write("\"RooExponential::signalPassExp(mass, expRatePass["+str(params.at(params.index("expRatePass")).getVal())+"])\",\n")
-                    out.write("\"RooExponential::signalFailExp(mass, expRateFail["+str(params.at(params.index("expRateFail")).getVal())+"])\",\n")
-                    out.write("\"SUM::signalPhyPass(cPass["+str(params.at(params.index("cPass")).getVal())+"]*signalPassBW, signalPassExp)\",\n")
-                    out.write("\"SUM::signalPhyFail(cFail["+str(params.at(params.index("cFail")).getVal())+"]*signalFailBW, signalFailExp)\",\n")
-                    out.write("\"RooCMSShape::backgroundPass(mass, alphaPass[60.,50.,70.], betaPass[0.001, 0.,0.1], gammaPass[0.1, 0, 1], peakPass[90.0])\",\n")
-                    out.write("\"RooCMSShape::backgroundFail(mass, alphaFail[60.,50.,70.], betaFail[0.001, 0.,0.1], gammaFail[0.1, 0, 1], peakFail[90.0])\",\n")
-                    out.write("\"FCONV::signalPass(mass, signalPhyPass, signalResPass)\",\n")
-                    out.write("\"FCONV::signalFail(mass, signalPhyFail, signalResFail)\",\n")
-                    out.write("\"efficiency[0.5,0,1]\",\n")
-                    out.write("\"signalFractionInPassing[1.0]\"\n")
-                    out.write("),\n")
-                    out.write("\n")
+                if "__" not in key.GetName():
+                    continue
+                subdirectory = directory.Get(key.GetName())
+                subdirectory.cd();
+                results = subdirectory.Get("fitresults")
+                params = results.floatParsFinal()
+                if entry == 100:
+                    entry = 0
+                    block += 1
+                    if block != 0:
+                        out.write(")\n")
+                    out.write("block_"+str(block)+" = cms.PSet(\n")
+                else:
+                    entry += 1
+                if GetFitName(filename, key.GetName()) == None:
+                    continue
+                print filename+" "+GetDirName(filename)+" "+key.GetName()+" "+GetFitName(filename, key.GetName())
+                out.write(GetFitName(filename, key.GetName())+" = cms.vstring(\n")
+                out.write("\"RooDoubleCBFast::signalResPass(mass,meanP[0.0,-10.000,10.000],sigmaP[0.956,0.00,10.000],alphaP1[0.999, 0.0,50.0],nP1[1.405,0.000,50.000],alphaP2[0.999,0.0,50.0],nP2[1.405,0.000,50.000])\",\n")
+                out.write("\"RooDoubleCBFast::signalResFail(mass,meanF[0.0,-10.000,10.000],sigmaF[3.331,0.00,10.000],alphaF1[1.586, 0.0,50.0],nF1[0.464,0.000,20.00],alphaF2[1.586,0.0,50.0],nF2[0.464,0.000,20.00])\",\n")
+                out.write("\"RooBreitWigner::signalPassBW(mass, mZpass[91.1876,80.,100.],sigmaZpass[2.4952,0.001,10.])\",\n")
+                out.write("\"RooBreitWigner::signalFailBW(mass, mZfail[91.1876,80.,100.],sigmaZfail[2.4952,0.001,10.])\",\n")
+                out.write("\"RooExponential::signalPassExp(mass, expRatePass["+str(params.at(params.index("expRatePass")).getVal())+"])\",\n")
+                out.write("\"RooExponential::signalFailExp(mass, expRateFail["+str(params.at(params.index("expRateFail")).getVal())+"])\",\n")
+                out.write("\"SUM::signalPhyPass(cPass["+str(params.at(params.index("cPass")).getVal())+"]*signalPassBW, signalPassExp)\",\n")
+                out.write("\"SUM::signalPhyFail(cFail["+str(params.at(params.index("cFail")).getVal())+"]*signalFailBW, signalFailExp)\",\n")
+                out.write("\"RooCMSShape::backgroundPass(mass, alphaPass[60.,50.,70.], betaPass[0.001, 0.,0.1], gammaPass[0.1, 0, 1], peakPass[90.0])\",\n")
+                out.write("\"RooCMSShape::backgroundFail(mass, alphaFail[60.,50.,70.], betaFail[0.001, 0.,0.1], gammaFail[0.1, 0, 1], peakFail[90.0])\",\n")
+                out.write("\"FCONV::signalPass(mass, signalPhyPass, signalResPass)\",\n")
+                out.write("\"FCONV::signalFail(mass, signalPhyFail, signalResFail)\",\n")
+                out.write("\"efficiency[0.5,0,1]\",\n")
+                out.write("\"signalFractionInPassing[1.0]\"\n")
+                out.write("),\n")
+                out.write("\n")
+        out.write(")\n")
+
+        out.write("all_pdfs = cms.PSet(\n")
+        for i in range(0,block+1):
+            out.write("block_"+str(i)+",\n")
+        out.write(")\n")
 
 if __name__ == "__main__":  
     parser = OptionParser()
