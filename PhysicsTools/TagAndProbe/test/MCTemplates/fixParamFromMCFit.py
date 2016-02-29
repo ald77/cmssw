9ba3aceb9ee670a6159a4ae234f2c8a7db07ca8a
Adam Dishaw <aldishaw@gmail.com>
2016-02-29 13:06:38 -0800

    Implement new parametric fit

--- PhysicsTools/TagAndProbe/test/MCTemplates/fixParamFromMCFit.py
+++ PhysicsTools/TagAndProbe/test/MCTemplates/fixParamFromMCFit.py
@@ -69,18 +69,10 @@ def GetFitName(filename, keyname):
             output = replace(output, "1p566To2p5", "2p0To2p5")
             output = replace(output, "0p0To2p5", "2p0To2p5")
         return output
+
+def Fix(name, params):
+    return name+"["+str(params.at(params.index(name)).getVal())+"]"
     
-def GetParamString(name, params):
-    for p in xrange(params):
-        param = params.at(p)
-        if param.GetName() == name:
-            if IsFixed(name):
-                return "%s[%.3f]"%(myPar.GetName(), myPar.getVal())
-            else:
-                return "%s[%.3f,%.3f,%.3f]"%(myPar.GetName(), myPar.getVal(), myPar.getMin(), myPar.getMax())
-
-    return None
-
 def main(options):
     with open("../python/commonFit.py", "w") as out:
         out.write("import FWCore.ParameterSet.Config as cms\n")
@@ -117,16 +109,20 @@ def main(options):
                 out.write(GetFitName(filename, key.GetName())+" = cms.vstring(\n")
                 out.write("\"RooDoubleCBFast::signalResPass(mass,meanP[0.0,-10.000,10.000],sigmaP[0.956,0.00,10.000],alphaP1[0.999, 0.0,50.0],nP1[1.405,0.000,50.000],alphaP2[0.999,0.0,50.0],nP2[1.405,0.000,50.000])\",\n")
                 out.write("\"RooDoubleCBFast::signalResFail(mass,meanF[0.0,-10.000,10.000],sigmaF[3.331,0.00,10.000],alphaF1[1.586, 0.0,50.0],nF1[0.464,0.000,20.00],alphaF2[1.586,0.0,50.0],nF2[0.464,0.000,20.00])\",\n")
-                out.write("\"RooBreitWigner::signalPassBW(mass, mZpass[91.1876,80.,100.],sigmaZpass[2.4952,0.001,10.])\",\n")
-                out.write("\"RooBreitWigner::signalFailBW(mass, mZfail[91.1876,80.,100.],sigmaZfail[2.4952,0.001,10.])\",\n")
-                out.write("\"RooExponential::signalPassExp(mass, expRatePass["+str(params.at(params.index("expRatePass")).getVal())+"])\",\n")
-                out.write("\"RooExponential::signalFailExp(mass, expRateFail["+str(params.at(params.index("expRateFail")).getVal())+"])\",\n")
-                out.write("\"SUM::signalPhyPass(cPass["+str(params.at(params.index("cPass")).getVal())+"]*signalPassBW, signalPassExp)\",\n")
-                out.write("\"SUM::signalPhyFail(cFail["+str(params.at(params.index("cFail")).getVal())+"]*signalFailBW, signalFailExp)\",\n")
+                out.write("\"RooBreitWigner::signalPassBWZ(mass, mZpass[91.1876,80.,100.],sigmaZpass[2.4952,0.001,10.])\",\n")
+                out.write("\"RooBreitWigner::signalFailBWZ(mass, mZfail[91.1876,80.,100.],sigmaZfail[2.4952,0.001,10.])\",\n")
+                out.write("\"RooBreitWigner::signalPassBWtail(mass, "+Fix("mtailpass",params)+","+Fix("sigmatailpass",params)+")\",\n")
+                out.write("\"RooBreitWigner::signalFailBWtail(mass, "+Fix("mtailfail",params)+","+Fix("sigmatailfail",params)+")\",\n")
+                out.write("\"RooGaussian::signalPassGaus(mass, "+Fix("meanVP",params)+","+Fix("sigmaVP",params)+")\",\n")
+                out.write("\"RooGaussian::signalFailGaus(mass, "+Fix("meanVF",params)+","+Fix("sigmaVF",params)+")\",\n")
+                out.write("\"FCONV::signalZPass(mass, signalPassBWZ, signalResPass)\",\n")
+                out.write("\"FCONV::signalZFail(mass, signalFailBWZ, signalResFail)\",\n")
+                out.write("\"FCONV::signalVoigtPass(mass, signalPassBWtail, signalPassGaus)\",\n")
+                out.write("\"FCONV::signalVoigtFail(mass, signalFailBWtail, signalFailGaus)\",\n")
+                out.write("\"SUM::signalPass("+Fix("cPass",params)+"*signalZPass,signalVoigtPass)\",\n")
+                out.write("\"SUM::signalFail("+Fix("cFail",params)+"*signalZFail,signalVoigtFail)\",\n")
                 out.write("\"RooCMSShape::backgroundPass(mass, alphaPass[60.,50.,70.], betaPass[0.001, 0.,0.1], gammaPass[0.1, 0, 1], peakPass[90.0])\",\n")
                 out.write("\"RooCMSShape::backgroundFail(mass, alphaFail[60.,50.,70.], betaFail[0.001, 0.,0.1], gammaFail[0.1, 0, 1], peakFail[90.0])\",\n")
-                out.write("\"FCONV::signalPass(mass, signalPhyPass, signalResPass)\",\n")
-                out.write("\"FCONV::signalFail(mass, signalPhyFail, signalResFail)\",\n")
                 out.write("\"efficiency[0.5,0,1]\",\n")
                 out.write("\"signalFractionInPassing[1.0]\"\n")
                 out.write("),\n")
