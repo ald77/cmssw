aaade4d52c6187d36b5a23cdbf3649ffd5729496
Adam Dishaw <aldishaw@gmail.com>
2016-03-02 11:40:58 -0800

    Make plots in batch mode

--- PhysicsTools/TagAndProbe/test/plotAll.sh
+++ PhysicsTools/TagAndProbe/test/plotAll.sh
@@ -2,8 +2,8 @@
 
 shopt -s nullglob
 
-./genSystText.py -n 2016_02_02_nominal -b 2016_02_05_expbkg -s 2016_03_01_param2 -m 2016_02_04_madgraph -t 2016_02_08_difftag
+./genSystText.py -n 2016_02_02_nominal -b 2016_02_05_expbkg -s 2016_03_01_param2 -m 2016_02_04_madgraph -t 2016_02_08_difftag &> syst_text.log
 for file in $(ls -A eff_all_*.txt)
 do
-    python plotting/EGammaID_scaleFactors.py $file
+    python plotting/EGammaID_scaleFactors.py $file -b
 done
