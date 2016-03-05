c28fb8bb97213f8d7ac68327328dfe4ca515b01a
Adam Dishaw <aldishaw@gmail.com>
2016-03-05 13:22:33 -0800

    Update default parametric fit

--- PhysicsTools/TagAndProbe/test/plotAll.sh
+++ PhysicsTools/TagAndProbe/test/plotAll.sh
@@ -2,7 +2,7 @@
 
 shopt -s nullglob
 
-./genSystText.py -n 2016_02_02_nominal -b 2016_02_05_expbkg -s 2016_03_01_param2 -m 2016_02_04_madgraph -t 2016_02_08_difftag &> syst_text.log
+#./genSystText.py -n 2016_02_02_nominal -b 2016_02_05_expbkg -s 2016_03_02_param2b -m 2016_02_04_madgraph -t 2016_02_08_difftag &> syst_text.log
 for file in $(ls -A eff_all_*.txt)
 do
     python plotting/EGammaID_scaleFactors.py $file -b
